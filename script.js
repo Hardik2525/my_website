f065f3b8f12402bc05bbc7cd75868adc73950696
Hardik2525 <86050644+Hardik2525@users.noreply.github.com>
2025-05-05 23:56:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMore() {
+  const description = document.getElementById("description");
+  description.textContent = "I'm [Your Name], a passionate individual with interests in [Your Interests or Profession]. I love building things with code, learning new tech, and helping others grow!";
+}
